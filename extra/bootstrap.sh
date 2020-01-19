55174ff1be5f927fc37270d9fc5af1370f906a8a
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-19 15:31:29 -0800

    Update bootstrap.sh

--- extra/bootstrap.sh
+++ extra/bootstrap.sh
@@ -17,7 +17,7 @@ dotfile_repo="https://www.github.com/qrbounty/dotfiles.git"
 # Print a horizontal rule
 # Source: https://brettterpstra.com/2015/02/20/shell-trick-printf-rules/
 rule () { printf -v _hr "%*s" $(tput cols) && echo ${_hr// /${1--}}; }
-
+right() { "printf '%*s' $(tput cols)"; }
 # Print a rule with a message in it
 rulem ()  {
   if [ $# -eq 0 ]; then
@@ -27,8 +27,6 @@ rulem ()  {
   # Fill line with ruler character ($2, default "-"), reset cursor, move 2 cols right, print message
   printf -v _hr "%*s" $(tput cols) && echo -en ${_hr// /${2--}} && echo -e "\r\033[2C$1"
 }
-
-alias right="printf '%*s' $(tput cols)"
 
 # Source: Modified from https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script 
 os () { [[ $OSTYPE == *$1* ]]; }
@@ -47,8 +45,8 @@ exists() { command -v "$1" >/dev/null 2>&1; }
 # Source: https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs
 error() { printf "$@\n" >&2; exit 1; }
 success() { printf "$@\n"; }
-log() { printf "$@\n"; }
-try() { rulem "$1" && "$2" && success "$3" || error "Failure at $1"; }
+log() { printf "-- $@\n"; }
+try() { log "$1" && "$2" && success "$3" || error "Failure at $1"; }
 
 # Usage: "apt_install vim"
 apt_install() {
@@ -56,15 +54,11 @@ apt_install() {
   sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq $1 < /dev/null > /dev/null && right "Installed!"
 }
 
-
-
-
-### Dependency Installation Variables ###
-declare -a debian_packages=("curl" "git" "python3" "python3-pip" "vim" "suckless-tools" "i3" "xorg" "xdm")
-declare -a pip3_packages=("yara")
+### Installer Functions ###
 
 debian_install() { 
   sudo apt-get update < /dev/null > /dev/null && echo "Packages updated"
+  declare -a debian_packages=("curl" "git" "python3" "python3-pip" "vim" "suckless-tools" "i3" "xorg" "xdm")
   for package in "${debian_packages[@]}"; do
     apt_install $package
   done
@@ -82,12 +76,14 @@ debian_install() {
 }
 
 pip3_packages() { 
+  declare -a pip3_packages=("yara")
   for package in "${pip3_packages[@]}"; do
     echo "Installing $package"
     pip3 install $package; > /dev/null
   done 
 }
 
+### Dotfile Stuff ###
 config(){ /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@; }
 dotfile_copy(){
   [ ! -d "$HOME/.cfg" ] && mkdir $HOME/.cfg
@@ -107,8 +103,9 @@ dotfile_copy(){
 
 ###  Main  ###
 rule "-"
-echo "Bootstrap Script Version Zero"
+printf "\nBootstrap Script Version Zero\n"
 rule "-"
+printf "\n"
 
 if os darwin; then
   if ! exists brew; then
@@ -119,7 +116,7 @@ if os darwin; then
 elif linux gnu; then
   if distro "Debian"; then
     rulem "Debian Customization" "~"
-    try "Installing Debian environment..." debian_install "Debian apps installed!"
+    try "Installing Debian environment..." debian_install "Debian environment set up!"
     try "Installing pip3 packages..." pip3_packages "Custom python packages installed!"
   fi
   if distro "Kali"; then
