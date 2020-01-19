5ae8d35a636754ac612cc4dd3657b07c75ac21e7
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-19 15:09:43 -0800

    Update bootstrap.sh

--- extra/bootstrap.sh
+++ extra/bootstrap.sh
@@ -10,18 +10,26 @@
 # TODO: Better "custom app" management. Right now just dumping them into functions.
 
 set -e
-
-###  Variables  ###
 dotfile_repo="https://www.github.com/qrbounty/dotfiles.git"
 
-### Dependency Installation Variables ###
-declare -a debian_packages=("curl" "git" "python3" "python3-pip" "vim" "suckless-tools" "i3" "xorg" "xdm")
-declare -a pip3_packages=("yara")
 
+### Helpers / Formatters ###
+# Print a horizontal rule
+# Source: https://brettterpstra.com/2015/02/20/shell-trick-printf-rules/
+rule () { printf -v _hr "%*s" $(tput cols) && echo ${_hr// /${1--}}; }
 
-###  Functions  ###
-# Usage: "if os darwin; then ..." or "if linux gnu; then ..."
-# Purpose: Quick check for os-specific functionality.
+# Print a rule with a message in it
+rulem ()  {
+  if [ $# -eq 0 ]; then
+    echo "Usage: rulem MESSAGE [RULE_CHARACTER]"
+  return 1
+  fi
+  # Fill line with ruler character ($2, default "-"), reset cursor, move 2 cols right, print message
+  printf -v _hr "%*s" $(tput cols) && echo -en ${_hr// /${2--}} && echo -e "\r\033[2C$1"
+}
+
+alias right="printf '%*s' $(tput cols)"
+
 # Source: Modified from https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script 
 os () { [[ $OSTYPE == *$1* ]]; }
 distro () { [[ $(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"') == *$1* ]]; }
@@ -33,15 +41,9 @@ linux () {
   [[ "${sys}" == "$1" ]];
 }
 
-
-# Usage: "if exists <app>; then ..." or "if ! exists..."
-# Purpose: A quick check to see if a program is installed. Not 100% reliable because it relies on $PATH
 # Source: https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
 exists() { command -v "$1" >/dev/null 2>&1; }
 
-
-# Usage: "try command 'Worked!'"
-# Purpose: A more customizable variant of the claw "yell, die, try"
 # Source: https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs
 error() { printf "$@\n" >&2; exit 1; }
 success() { printf "$@\n"; }
@@ -49,11 +51,17 @@ log() { printf "$@\n"; }
 try() { rulem "$1" && "$2" && success "$3" || error "Failure at $1"; }
 
 # Usage: "apt_install vim"
-# Purpose: Simple wrapper for quieter installs
 apt_install() {
   printf "Installing package $1 / " 
   sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq $1 < /dev/null > /dev/null && right "Installed!"
 }
+
+
+
+
+### Dependency Installation Variables ###
+declare -a debian_packages=("curl" "git" "python3" "python3-pip" "vim" "suckless-tools" "i3" "xorg" "xdm")
+declare -a pip3_packages=("yara")
 
 debian_install() { 
   sudo apt-get update < /dev/null > /dev/null && echo "Packages updated"
@@ -95,24 +103,6 @@ dotfile_copy(){
   config checkout
   config config status.showUntrackedFiles no
 }
-
-
-# Print a horizontal rule
-# Source: https://brettterpstra.com/2015/02/20/shell-trick-printf-rules/
-rule () { printf -v _hr "%*s" $(tput cols) && echo ${_hr// /${1--}}; }
-
-# Print a rule with a message in it
-rulem ()  {
-  if [ $# -eq 0 ]; then
-    echo "Usage: rulem MESSAGE [RULE_CHARACTER]"
-  return 1
-  fi
-  # Fill line with ruler character ($2, default "-"), reset cursor, move 2 cols right, print message
-  printf -v _hr "%*s" $(tput cols) && echo -en ${_hr// /${2--}} && echo -e "\r\033[2C$1"
-}
-
-alias right="printf '%*s' $(tput cols)"
-
 
 
 ###  Main  ###
