3fbeefc49dd5376e4c81f08a535845385eaaead9
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-19 12:32:33 -0800

    Update bootstrap.sh

--- extra/bootstrap.sh
+++ extra/bootstrap.sh
@@ -4,6 +4,7 @@
 #
 # TODO: OSX install tasks
 # TODO: Custom git repos
+# TODO: Add dry run functionality
 # TODO: Better "custom app" management. Right now just dumping them into functions.
 
 
@@ -12,13 +13,15 @@ dotfile_repo="https://www.github.com/qrbounty/dotfiles.git"
 text_bar="~~~-----------------------------------------------------------~~~"
 
 ### Dependency Installation Variables ###
-declare -a debian_packages=("git" "python3" "python3-pip" "vim" "i3" "xorg" "suckless-tools" "lightdm")
+declare -a debian_packages=("git" "python3" "python3-pip" "vim" "suckless-tools")
+declare -a i3_packages=("i3" "xorg" "xdm")
 declare -a pip3_packages=("yara")
+
 
 ###  Functions  ###
 # Usage: "if os darwin; then ..." or "if linux gnu; then ..."
 # Purpose: Quick check for os-specific functionality.
-# Source: Modified fromhttps://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script 
+# Source: Modified from https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script 
 os () { [[ $OSTYPE == *$1* ]]; }
 distro () { [[ $(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"') == *$1* ]]; }
 linux () { 
@@ -29,10 +32,12 @@ linux () {
   [[ "${sys}" == "$1" ]];
 }
 
+
 # Usage: "if exists <app>; then ..." or "if ! exists..."
 # Purpose: A quick check to see if a program is installed. Not 100% reliable because it relies on $PATH
 # Source: https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
 exists() { command -v "$1" >/dev/null 2>&1; }
+
 
 # Usage: "try command 'Worked!'"
 # Purpose: A more customizable variant of the claw "yell, die, try"
@@ -41,18 +46,16 @@ fmt() { printf "\n$text_bar\n$(date +'%H:%M:%S'):"; }
 err() { printf "$(fmt) $@\n" >&2; exit 1; }
 yay() { printf "$@\n"; }
 log() { printf "$(fmt) $@\n$text_bar\n"; }
-try() { "$1" && yay "$2" || err "Failure at $1"; }
+try() { log "$1" && "$2" && yay "$3" || err "Failure at $1"; }
+
 
 debian_install() { 
-  echo "Updating system..."
-  sudo apt-get update > /dev/null
+  sudo apt-get update
   for package in "${debian_packages[@]}"; do
-    echo "Installing $package ..."
-    sudo apt-get -qq -o Dpkg::Use-Pty=0 install -y $package > /dev/null; 
+    echo "Installing $package"
+    sudo apt-get install -y $package > /dev/null; 
   done
-  echo "Setting i3 as default WM"
-  echo "exec i3" > $HOME/.xsession
-  update-alternatives --config x-session-manager
+  echo 'exec i3' > ~/.xsession
 }
 
 pip3_packages() { 
@@ -90,18 +93,18 @@ if os darwin; then
     log "Installing Brew..."
   fi 
 elif linux gnu; then
+  log "Detected OS: Linux"
   if distro "Debian"; then
-    log "Running Debian Install Scripts"
-    try debian_install "Debian apps installed"
-    log "Installing pip3 packages..."
-    try pip3_packages "Custom python packages installed"
+    log "Detected Distro: Debian"
+    try "Installing Debian base apps..." debian_install "Debian apps installed"
+    try "Installing i3 + xdm environment..." 
+    try "Installing pip3 packages..." pip3_packages "Custom python packages installed"
   fi
   if distro "Kali"; then
-    log "Running Kali Install Scripts"
+    log "Detected Distro: Kali"
   fi
   if exists git; then
-    log "Grabbing dotfiles. Conflicts will be saved to .config-backup"
-    try dotfile_copy "Dotfile repo cloned."
+    try "Grabbing dotfiles. Conflicts will be saved to .config-backup..." dotfile_copy "Dotfile repo cloned"
   else
     err "git not detected, cannot gather dotfiles."
   fi
