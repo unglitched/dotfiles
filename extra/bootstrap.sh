6236b691f13eaefc1d9545750149e5b298f3dcda
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-19 16:00:09 -0800

    Update bootstrap.sh

--- extra/bootstrap.sh
+++ extra/bootstrap.sh
@@ -66,7 +66,7 @@ debian_install() {
   
   # VS Code install
   rulem "Installing VS Code"
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
   sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
   sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
   apt_install apt-transport-https
