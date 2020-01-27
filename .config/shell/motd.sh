326c328886f2aac1382ab7aa17f4985515bc173d
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-26 19:17:19 -0800

    Update motd.sh

--- .config/shell/motd.sh
+++ .config/shell/motd.sh
@@ -1,18 +1,16 @@
-b=$(tput bold)
-nm=$(tput sgr0)
 echo "
 Don't forget:
-  ${b}conf-*${nm}    commands are shortcuts to edit your dotfiles.
-  ${b}config${nm}    is a git alias for the dotfiles repo
+  conf-*    commands are shortcuts to edit your dotfiles.
+  config    is a git alias for the dotfiles repo
 
 Useful Apps:
-  ${b}Terminal${nm}
-    ${b}tldr${nm}    shorter manpages, with examples
-    ${b}ripgrep${nm} grep, but way faster
-    ${b}ranger${nm}  file browser
+  Terminal
+    tldr    shorter manpages, with examples
+    ripgrep grep, but way faster
+    ranger  file browser
 
-  ${b}Reverse Engineering${nm}
-    ${b}radare2${nm}  A nice and modern debugger
-    ${b}gdb${nm}      the GNU debugger
-    ${b}binwalk${nm}  binary analysis and extraction tool
+  Reverse Engineering${nm}
+    radare2  A nice and modern debugger
+    gdb      the GNU debugger
+    binwalk  binary analysis and extraction tool
 "
