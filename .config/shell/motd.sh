236bc368ad32835004cea79c1a6b7960d0619216
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-26 21:05:20 -0800

    Update motd.sh

--- .config/shell/motd.sh
+++ .config/shell/motd.sh
@@ -2,15 +2,25 @@ echo "
 Don't forget:
   conf-*    commands are shortcuts to edit your dotfiles.
   config    is a git alias for the dotfiles repo
+  rofi      can be activated with Cmd+d
 
-Useful Apps:
+Useful Custom Apps:
   Terminal
     tldr    shorter manpages, with examples
     ripgrep grep, but way faster
     ranger  file browser
 
-  Reverse Engineering${nm}
+  Development
+    code    Visual Studio Code, a fully featured IDE
+    
+  Reverse Engineering
     radare2  A nice and modern debugger
     gdb      the GNU debugger
     binwalk  binary analysis and extraction tool
+    flashrom A rom flashing tool
+  
+  Password Cracking and Fuzzing
+    hashcat  The go-to password cracking tool
+    afl      The go-to fuzzing suite
+    zzuf     A simple fuzzing tool
 "
